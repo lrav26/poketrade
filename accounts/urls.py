ed0d43326257f942e6bdbd934e18cb759d88b0bd
Camilo Castrillon <cam.castrillon23@outlook.com>
2025-04-11 11:33:17 -0400

    merged collections to test

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
-path('logout/', views.logout, name='accounts.logout'),
-path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('logout/', views.logout, name='accounts.logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
