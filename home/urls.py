0fce0ae9034eb177935da869699c03f36a7d1e00
Camilo Castrillon <cam.castrillon23@outlook.com>
2025-04-18 12:00:03 -0400

    More Collections fixes

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
-    path('collection/', views.collection, name='collection'),  # This is the collection page route
+    path('collection/', views.collection, name='collection'),
     path('pokemon/<int:id>/', views.pokemon_detail, name='pokemonDetail'),
     path('nickname_update/<int:pk>/', views.nickname_update, name='nickname_update'),
     path('logs/', views.logs, name='logs'),
