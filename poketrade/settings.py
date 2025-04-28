34891b6480fedb5d231d80a02636895d03c65253
zohaalam <zalam.7505@gmail.com>
2025-04-28 02:52:34 -0400

    trying to deploy website

--- poketrade/settings.py
+++ poketrade/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / 'poketrade/static/',
-]
+#STATICFILES_DIRS = [
+ #   BASE_DIR / "static",
+  #  BASE_DIR / 'poketrade/static/',
+#]
