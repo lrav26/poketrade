48f050c4fb7594b1c075fa3fbfc3924e0fa361bd
zohaalam <zalam.7505@gmail.com>
2025-04-28 03:56:14 -0400

    trying to deploy website

--- poketrade/settings.py
+++ poketrade/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = ['zohaa.pythonanywhere.com']
-
+ALLOWED_HOSTS = ['zohaa.pythonanywhere.com',
+                 'localhost', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
