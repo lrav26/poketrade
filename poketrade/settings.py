c526f1aac7ceaa0a7f1c1f20007c7e94ee99dc39
zohaalam <zalam.7505@gmail.com>
2025-04-28 03:29:58 -0400

    trying to deploy website

--- poketrade/settings.py
+++ poketrade/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u4%e+s_s7!#0s*d@qwmrkoq4)-g90cuc)b6ik#1zsbk=eeh9rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/collection/'
